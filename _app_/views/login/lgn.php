57714ef895e1a154d2dc8657a9c1020a12fcee39
navtewari <navtewari@gmail.com>
2016-03-22 20:51:18 +0530

    updates
    updates Login

--- _app_/views/login/lgn.php
+++ _app_/views/login/lgn.php
@@ -1,78 +1,81 @@
 <div class="page">
     <div class="container">
-        <div class="col-md-12 detail">  
-            <h3 class="tittle">Login Please</h3>
-                <div class="contact-form">
-                    <div class="col-sm-6">  
-                        <?php echo form_open('web/sign_in_', array('name' => "frmLogin_", 'id' => "frmLogin_", 'class' => 'comment-form-author', 'style' => 'width: 100%')); ?>
+        <div class="row">  
+            <h2>Login Please</h2>
+            <div class="contact-form">
+                <div class="row">  
+                    <div class="col-sm-4"></div>
+                    <div class="col-sm-4">  
+                        <?php echo form_open('web/sign_in_', array('name' => "frmLogin_", 'id' => "frmLogin_", 'class' => 'form', 'style' => 'width: 100%')); ?>
                         <p class="comment-form-author"><label for="author">Username<span class="mendatory1">*</span></label>
                             <?php
                             $data = array(
                                 'type' => 'text',
                                 'required' => 'required',
-                                'class' => 'textbox',
+                                'class' => 'form-control',
                                 'name' => 'txtUsr',
                                 'id' => 'txtUsr',
                                 'placeholder' => 'Username here...',
-                                'class' => 'search-text',
                                 'style' => 'width: 100%'
                             );
                             echo form_input($data);
                             ?>
                         </p>
                     </div>
-                    <div class="col-sm-6">  
+                    <div class="col-sm-4"></div>
+                </div>
+                <div class="row">  
+                    <div class="col-sm-4"></div>
+                    <div class="col-sm-4">  
                         <p class="comment-form-author"><label for="author">Password<span class="mendatory1">*</span></label>
                             <?php
                             $data = array(
                                 'type' => 'password',
                                 'required' => 'required',
-                                'class' => 'textbox',
+                                'class' => 'form-control',
                                 'name' => 'txtPwd',
                                 'id' => 'txtPwd',
                                 'placeholder' => 'Password here...',
-                                'class' => 'search-text',
                                 'style' => 'width: 100%'
                             );
                             echo form_input($data);
                             ?>
                         </p>
                     </div>
-                    <div class="col-sm-6"> </div>
-                    <div class="col-sm-6"> 
+                    <div class="col-sm-4"></div>
+                </div>
+                <div class="row" style="margin-top:20px;">  
+                    <div class="col-sm-4"></div>
+                    <div class="col-sm-4"> 
                         <div class="col-sm-6"> 
-                            <p style="float: left; color: #ff0000" id="login_msg"></p>
-                            <?php
+                            <p class="comment-form-author">
+                                <?php
                                 $data = array(
                                     'name' => 'submit',
                                     'type' => 'submit',
-                                    'class' => 'col-sm-12',
+                                    'class' => 'col-xs-12 col-sm-12 btn btn-primary',
                                     'value' => 'Sign in'
                                 );
                                 echo form_input($data);
-                            ?>
+                                ?>
                         </div>
                         <div class="col-sm-6"> 
                             <?php
-                                $data = array(
-                                    'name' => 'reset',
-                                    'type' => 'reset',
-                                    'class' => 'col-sm-12',
-                                    'value' => 'Reset'
-                                );
-                                echo form_input($data);
+                            $data = array(
+                                'name' => 'reset',
+                                'type' => 'reset',
+                                'class' => 'col-xs-12 col-sm-12 btn btn-danger',
+                                'value' => 'Reset'
+                            );
+                            echo form_input($data);
                             ?>
                         </div>
                     </div>
-                    <?php echo form_close(); ?>
+                    <div class="col-sm-4"></div>
                 </div>
+                <?php echo form_close(); ?>
             </div>
-            <div class="col-md-4">
-                <p>&nbsp;</p>
-            </div>
-            <div class="col-md-4">
-                <?php //$this->load->view('templates/imageCrousel');  ?>
-            </div>        
-            <div class="clearfix"> </div>
-        </div>    
+        </div>        
+        <div class="clearfix"> </div>
+    </div>    
 </div>
--- _app_/views/templates/innerBanner.php
+++ _app_/views/templates/innerBanner.php
@@ -1,31 +1,12 @@
-<script src="<?php echo base_url('_assets_/js/responsiveslides.min.js'); ?>"></script>
-<script>
-    $(function () {
 
-        $("#slider4").responsiveSlides({
-            auto: true,
-            pager: true,
-            nav: true,
-            speed: 500,
-            namespace: "callbacks",
-            before: function () {
-                $('.events').append("<li>before event fired.</li>");
-            },
-            after: function () {
-                $('.events').append("<li>after event fired.</li>");
-            }
-        });
-
-    });
-</script>
 <div  id="top" class="callbacks_container" style="margin-top:100px;">
     <ul class="rslides" id="slider4">
         <li>
-            <img src="<?php echo base_url('_assets_/images/slide1.jpg'); ?>" alt="">
-            <div class="caption">
-                <div class="slide-text-info">
-                    <h1>providing</h1>
-                    <label>highquality education for Children</label>
+            <img src="<?php echo base_url('_assets_/images/slide4.jpg'); ?>" alt="">
+            <div class="caption1">
+                <div class="slide-text-info1">
+                    <h1>Login</h1>
+                    <label>Panel for updating dynamic content</label>
                 </div>
             </div>
         </li>
