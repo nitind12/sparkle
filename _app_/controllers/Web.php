1c89e1ec2bde5d4cccd21200b6a9612b1c99825e
Nitin Deepak <nitind12@users.noreply.github.com>
2016-03-22 15:37:51 +0530

    Update Web.php

--- _app_/controllers/Web.php
+++ _app_/controllers/Web.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -29,35 +28,3 @@ class Web extends CI_Controller {
         }
     }
 }
-=======
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Web extends CI_Controller {
-	public function index()
-	{
-		$this->load->view('templates/header');
-		$this->load->view('home');
-		$this->load->view('templates/footer');
-	}
-    function login(){
-        $this->load->view('templates/header');
-        $this->load->view('login/lgn');
-        $this->load->view('templates/footer');
-    }
-    function uc_() {
-        $this->load->view('index');
-    }
-	function sign_in_() {
-        $res_ = $this->mm->login();
-
-        if ($res_['bool_'] == TRUE) {
-            $this->session->set_userdata('ussr_', $this->input->post('txtUsr'));
-            $this->session->set_userdata('stss_', $res_['sts_']);
-            redirect('/admin_');
-        } else {
-            redirect('../');
-        }
-    }
-}
->>>>>>> origin/master
