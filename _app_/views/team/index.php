c9d0f24a1947c4be65c38b6d9c9a01f6a0dbf91e
navtewari <navtewari@gmail.com>
2016-03-22 12:52:30 +0530

    updates
    updates

--- _app_/views/team/index.php
+++ _app_/views/team/index.php
@@ -1,53 +1,56 @@
 <div id="team" class="team">
-				<div class="container">
-					<div class="header team-head text-center">
-						<h2>Quick Links</h2>
-						<p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
-					</div>
-					<!--- teammember-grids ---->
-					<div class="team-member-grids">
-						<div class="team-member-grid">
-							<img src="<?php echo base_url('_assets_/images/t1.jpg');?>" title="name" />
-							<div class="team-member-info bottom-t-info">
-								<span> </span>
-								<h3><a href="#">Director's Message</a></h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. when an unknown printer took a galley of type Lorem Ipsum is simply dummy text.is simply dummy text..is simply dummy text.</p>
-							</div>
-						</div>
-						<div class="team-member-grid">
-							<div class="team-member-info bottom-t-info bottom-t-info-b">
-								<span> </span>
-								<h3><a href="#">Principal's Message</a></h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. when an unknown printer took a galley of type Lorem Ipsum is simply dummy text.is simply dummy text..is simply dummy text..is simply dummy text.</p>
-							</div>
-							<img src="<?php echo base_url('_assets_/images/t2.jpg');?>" title="name" />
-						</div>
-						<div class="team-member-grid">
-							<img src="<?php echo base_url('_assets_/images/t3.jpg');?>" title="name" />
-							<div class="team-member-info bottom-t-info">
-								<span> </span>
-								<h3><a href="#">Happy Birthday</a></h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. when an unknown printer took a galley of type Lorem Ipsum is simply dummy text.is simply dummy text..is simply dummy text.</p>
-							</div>
-						</div>
-						<div class="team-member-grid">
-							<div class="team-member-info bottom-t-info bottom-t-info-b">
-								<span> </span>
-								<h3><a href="#">Important Links</a></h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. when an unknown printer took a galley of type Lorem Ipsum is simply dummy text.is simply dummy text..is simply dummy text..is simply dummy text.</p>
-							</div>
-							<img src="<?php echo base_url('_assets_/images/t4.jpg');?>" title="name" />
-						</div>
-						<div class="team-member-grid">
-							<img src="<?php echo base_url('_assets_/images/t5.jpg');?>" title="name" />
-							<div class="team-member-info bottom-t-info">
-								<span> </span>
-								<h3><a href="#">PhotoGallery</a></h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. when an unknown printer took a galley of type Lorem Ipsum is simply dummy text.is simply dummy text..is simply dummy text.</p>
-							</div>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-					<!---//teammember-grids ---->
-				</div>
-			</div>
+    <div class="container">
+        <div class="header team-head text-left">
+            <h2>Quick Links</h2>
+        </div>
+        <!--- teammember-grids ---->
+        <div class="team-member-grids">
+            <div class="team-member-grid">
+                <img src="<?php echo base_url('_assets_/images/t1.jpg'); ?>" title="name" />
+                <div class="team-member-info bottom-t-info">
+                    <span> </span>
+                    <h3>Director's Message</h3>
+                    <p>Our vision is of a natural, holistic, student-centered learning environment that empowers and inspires our students to be creative. The Mission of our School is to ... </p>
+                </div>
+            </div>
+            <div class="team-member-grid">
+                <div class="team-member-info bottom-t-info bottom-t-info-b">
+                    <span> </span>
+                    <h3>Principal's Message</h3>
+                    <p>Globalization demands a new dimension to our approach to education. We, thus have to recognize the need for a globally relevant education. Such an education would mean imparting skills that would develop...</p>
+                </div>
+                <img src="<?php echo base_url('_assets_/images/t2.jpg'); ?>" title="name" />
+            </div>
+            <div class="team-member-grid">
+                <img src="<?php echo base_url('_assets_/images/t3.jpg'); ?>" title="name" />
+                <div class="team-member-info bottom-t-info">
+                    <span> </span>
+                    <h3>Happy Birthday</h3>
+                    <p>Wishing you a Happy Birthday. May GOD bless you all the days you live.</p>
+                </div>
+            </div>
+            <div class="team-member-grid">
+                <div class="team-member-info bottom-t-info bottom-t-info-b">
+                    <span> </span>
+                    <h3>Important Links</h3>
+                    <a href="http://www.cbse.nic.in" target="_blank">www.cbse.nic.in</a>
+                    <a href="http://www.cbseresults.nic.in" target="_blank">www.cbseresults.nic.in</a>
+                    <a href="http://www.ncert.nic.in" target="_blank">www.ncert.nic.in</a>
+                    <a href="http://www.indiaresults.com" target="_blank">www.indiaresults.com</a>
+                    <a href="http://www.india.gov.in" target="_blank">www.india.gov.in</a>
+                </div>
+                <img src="<?php echo base_url('_assets_/images/t4.jpg'); ?>" title="name" style="margin-top:48px;"/>
+            </div>
+            <div class="team-member-grid">
+                <img src="<?php echo base_url('_assets_/images/t5.jpg'); ?>" title="name" />
+                <div class="team-member-info bottom-t-info">
+                    <span> </span>
+                    <h3>PhotoGallery</h3>
+                    <p>Here you will find a large collection of some of our best photos to give you a peek at some of our amazing infrastructure, a sense of the activities within.</p>
+                </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+        <!---//teammember-grids ---->
+    </div>
+</div>
