43ce601c8b5097cec2ed0569a082bb1e14471209
navtewari <navtewari@gmail.com>
2016-03-22 15:25:45 +0530

    update
    update

--- _app_/controllers/Web.php
+++ _app_/controllers/Web.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Web extends CI_Controller {
-	public function index()
-	{
-		$this->load->view('templates/header');
-		$this->load->view('home');
-		$this->load->view('templates/footer');
-	}
+
+    public function index() {
+        $this->load->view('templates/header');
+        $this->load->view('home');
+        $this->load->view('templates/footer');
+    }
 }
---
+++ _app_/views/about/about.php
@@ -0,0 +1,6 @@
+
+			<?php $this -> load -> view ('templates/slider'); ?>
+			
+			
+			<?php $this -> load -> view('about/index'); ?>
+			<?php $this -> load -> view ('team/index'); ?>
